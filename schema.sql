3862f0793cd6009d3caff3b27383cf045905a06e
LU99IS99 <133922082+LU99IS99@users.noreply.github.com>
2025-02-03 21:36:02 -0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -10,3 +10,17 @@ CREATE TABLE IF NOT EXISTS products (
   description TEXT,
   productImage TEXT
 );
+
+DROP TABLE IF EXISTS inventory;
+CREATE TABLE IF NOT EXISTS inventory (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  productName TEXT NOT NULL,
+  harvestedDate DATE NOT NULL,
+  quality TEXT,
+  quantity INTEGER NOT NULL,
+  packUnit TEXT NOT NULL,
+  pricePerUnit DECIMAL(10, 2) NOT NULL,
+  sku TEXT NOT NULL,
+  continueSellingWhenOutOfStock BOOLEAN DEFAULT false,
+  notifyWhenInventoryLessThan INTEGER DEFAULT 10
+);
